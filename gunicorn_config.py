37a2bbf2896c80694cdbe09c7f45c72ddfc48df2
TimMcCauley <timothy.ellersiek@uni-heidelberg.de>
2018-08-06 13:29:18 +0200

    Replacing sync in favor of gevent flask, closes #30

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,7 @@
 bind = "0.0.0.0:5000"
 # Generally we recommend (2 x $num_cores) + 1 as the number of workers to start off with
-workers = 2
+workers = 4
+worker_class = 'gevent'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
