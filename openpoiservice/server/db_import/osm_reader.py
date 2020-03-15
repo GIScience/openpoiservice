b0286cdc21eeceada7c7db064928156e47b90124
nilsnolde <nils.nolde@gmail.com>
2020-03-15 18:15:20 +0800

    pyosmium works for now, needs some looking at memory consumption

---
+++ openpoiservice/server/db_import/osm_reader.py
@@ -0,0 +1,156 @@
+import osmium
+from shapely import wkb as wkblib, geometry
+import logging
+import uuid
+from copy import deepcopy
+
+from openpoiservice.server import categories_tools, ops_settings, db
+from openpoiservice.server.db_import.models import Pois, Tags, Categories
+from openpoiservice.server.db_import.objects import PoiObject, TagsObject
+
+wkb_fac = osmium.geom.WKBFactory()
+wkt_fac = osmium.geom.WKTFactory()
+logger = logging.getLogger(__name__)
+
+# TODO: Use logic to pick best memory option based on
+class OsmReader(osmium.SimpleHandler):
+    """Let's see where this leads.."""
+
+    def __init__(self):
+        super().__init__()
+        self._poi_objects = list()
+        self._tag_objects = list()
+        self._cat_objects = list()
+
+        # Counters
+        self.poi_count = 0
+
+    @staticmethod
+    def extract_tags(tags):
+        """
+        Takes a :class:`osmium.osm.TagList` object and returns as dict
+
+        :param tags: The tags returned from osmium.
+        :type tags: osmium.osm.TagList
+
+        :rtype: dict
+        """
+        tags_dict = dict()
+        for tag in tags:
+            tags_dict[tag.k] = tag.v
+        return tags_dict
+
+    def node(self, obj_node):
+        """
+        Call back for :class:`osmium.osm.Node` parser.
+
+        :param obj_node: The node currently processed.
+        :type obj_node: osmium.osm.Node
+        """
+        cat_included = categories_tools.get_category(obj_node.tags)
+        if cat_included:
+            osmid = deepcopy(obj_node.id)
+            tags = self.extract_tags(obj_node.tags)
+            location = wkt_fac.create_point(obj_node.location)
+
+            self.store_objects(
+                osmid=osmid,
+                tags=tags,
+                location=location,
+                osm_type=1,
+                categories=cat_included
+            )
+
+    def area(self, obj_area):
+        """
+        Callback for :class:`osmium.osm.Area` parser
+
+        :param obj_area: The current area being processed.
+        :type obj_area: osmium.osm.Area
+        """
+        cat_included = categories_tools.get_category(obj_area.tags)
+        if cat_included:
+            if obj_area.from_way():
+                osm_type = 2
+            else:
+                osm_type = 3
+
+            osmid = deepcopy(obj_area.id)
+            tags = self.extract_tags(obj_area.tags)
+            # Determine centroid
+            # TODO: import whole polygons, maybe simplified
+            wkb_area = wkb_fac.create_multipolygon(obj_area)
+            geom_multipolygon = wkblib.loads(wkb_area, hex=True)
+
+            for poly in geom_multipolygon:
+                self.store_objects(
+                    osmid=osmid,
+                    tags=tags,
+                    location=poly.centroid.wkt,
+                    osm_type=osm_type,
+                    categories=cat_included
+                )
+
+        pass
+
+    def store_objects(self, osmid, tags, location, osm_type, categories):
+        """
+        Creates the model objects and saves them periodically.
+
+        :type osmid: int
+        :type tags: dict
+        :type location: list|tuple[int]
+        :type osm_type: int
+
+        :param categories: The valid categories for each POI
+        :type categories: list
+        """
+        self.poi_count += 1
+        # TODO: get rid of uuid, seems pointless as PK (which is not even true other than the OSM object table)
+        # random id used as primary key
+        uid = uuid.uuid4().bytes
+
+        # Create and store tags dynamically from settings yml
+        # TODO: since tags table is 1:1 with OSM objects, this doesn't have to be its own table
+        # consider putting it in a JSON column instead in the OSM object table
+        for tag_key, tag_value in tags.items():
+            if tag_key in ops_settings['column_mappings']:
+                self._tag_objects.append(Tags(
+                    uuid=uid,
+                    osm_id=osmid,
+                    key=tag_key,
+                    value=tag_value
+                ))
+
+        # Create POI object
+        self._poi_objects.append(Pois(
+            uuid=uid,
+            osm_id=osmid,
+            osm_type=osm_type,
+            geom=location
+        ))
+
+        # Create categories
+        # TODO: Categories should only be stored once. Now it's creating a quadrillion useless copies
+        for category in categories:
+            self._cat_objects.append(Categories(
+                uuid=uid,
+                category=category
+            ))
+
+        # periodically dump the objects in the DB
+        if len(self._poi_objects) % 10000 == 0:
+            logger.info(f"Saved {len(self._poi_objects)} POIs")
+            self.save_objects()
+
+    def save_objects(self):
+        """
+        Saves the objects in the DB.
+        """
+        db.session.add_all(self._poi_objects)
+        db.session.add_all(self._cat_objects)
+        db.session.add_all(self._tag_objects)
+        db.session.commit()
+        self._poi_objects.clear()
+        self._tag_objects.clear()
+        self._cat_objects.clear()
--- openpoiservice/server/db_import/parser.py
+++ openpoiservice/server/db_import/parser.py
@@ -1,5 +1,6 @@
 # openpoiservice/server/parser.py
 
+from .osm_reader import OsmReader
 from openpoiservice.server.db_import.parse_osm import OsmImporter
 from openpoiservice.server.utils.decorators import timeit, processify
 from openpoiservice.server import ops_settings
@@ -16,51 +17,63 @@ logger = logging.getLogger(__name__)
 
 # process this function to free memory after import of each osm file
 
+# def parse_import(osm_file):
+#     logger.info('Starting to read {}'.format(osm_file))
+#
+#     osm_importer = OsmImporter()
+#
+#     logger.info('Parsing and importing nodes...')
+#     nodes = OSMParser(concurrency=ops_settings['concurrent_workers'], nodes_callback=osm_importer.parse_nodes)
+#     nodes.parse(osm_file)
+#
+#     logger.info('Parsing relations...')
+#     relations = OSMParser(concurrency=ops_settings['concurrent_workers'],
+#                           relations_callback=osm_importer.parse_relations)
+#     relations.parse(osm_file)
+#     logger.info('Found {} ways in relations'.format(osm_importer.relations_cnt))
+#
+#     logger.info('Parsing ways...')
+#     ways = OSMParser(concurrency=ops_settings['concurrent_workers'], ways_callback=osm_importer.parse_ways)
+#     ways.parse(osm_file)
+#
+#     logger.info('Found {} ways'.format(osm_importer.ways_cnt))
+#     del osm_importer.relation_ways
+#
+#     # Sort the ways by the first osm_id reference, saves memory for parsing coords
+#     osm_importer.process_ways.sort(key=lambda x: x.refs[0])
+#     # init self.process_ways_length before the first call of parse_coords function!
+#     osm_importer.process_ways_length = len(osm_importer.process_ways)
+#
+#     # https://docs.python.org/3/library/collections.html#collections.deque
+#     osm_importer.process_ways = deque(osm_importer.process_ways)
+#     logger.info('Importing ways... (note this wont work concurrently)')
+#
+#     coords = OSMParser(concurrency=1, coords_callback=osm_importer.parse_coords_for_ways)
+#     coords.parse(osm_file)
+#
+#     logger.info('Storing remaining pois')
+#     osm_importer.save_remainder()
+#
+#     logger.info('Found {} pois'.format(osm_importer.pois_cnt))
+#
+#     logger.info('Finished import of {}'.format(osm_file))
+#
+#     # logger.debug('Heap: {}'.format(h.heap()))
+#
+#     # clear memory
+#     del osm_importer
+
+
 def parse_import(osm_file):
-    logger.info('Starting to read {}'.format(osm_file))
+    reader = OsmReader()
 
-    osm_importer = OsmImporter()
+    logger.info(f"Starting with file {osm_file}...")
+    reader.apply_file(osm_file, locations=True, idx='sparse_mmap_array')
 
-    logger.info('Parsing and importing nodes...')
-    nodes = OSMParser(concurrency=ops_settings['concurrent_workers'], nodes_callback=osm_importer.parse_nodes)
-    nodes.parse(osm_file)
+    logger.info(f"Found {reader.poi_count} POIs")
 
-    logger.info('Parsing relations...')
-    relations = OSMParser(concurrency=ops_settings['concurrent_workers'],
-                          relations_callback=osm_importer.parse_relations)
-    relations.parse(osm_file)
-    logger.info('Found {} ways in relations'.format(osm_importer.relations_cnt))
-
-    logger.info('Parsing ways...')
-    ways = OSMParser(concurrency=ops_settings['concurrent_workers'], ways_callback=osm_importer.parse_ways)
-    ways.parse(osm_file)
-
-    logger.info('Found {} ways'.format(osm_importer.ways_cnt))
-    del osm_importer.relation_ways
-
-    # Sort the ways by the first osm_id reference, saves memory for parsing coords
-    osm_importer.process_ways.sort(key=lambda x: x.refs[0])
-    # init self.process_ways_length before the first call of parse_coords function!
-    osm_importer.process_ways_length = len(osm_importer.process_ways)
-
-    # https://docs.python.org/3/library/collections.html#collections.deque
-    osm_importer.process_ways = deque(osm_importer.process_ways)
-    logger.info('Importing ways... (note this wont work concurrently)')
-
-    coords = OSMParser(concurrency=1, coords_callback=osm_importer.parse_coords_for_ways)
-    coords.parse(osm_file)
-
-    logger.info('Storing remaining pois')
-    osm_importer.save_remainder()
-
-    logger.info('Found {} pois'.format(osm_importer.pois_cnt))
-
-    logger.info('Finished import of {}'.format(osm_file))
-
-    # logger.debug('Heap: {}'.format(h.heap()))
-
-    # clear memory
-    del osm_importer
+    # Save whatever's left in the object lists
+    reader.save_objects()
 
 
 @processify
